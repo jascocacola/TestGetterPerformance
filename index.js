8245b764afd20b50d23d128e9630acc7cf48d3d5
Jason Jaskolka <jjaskolka+github@gmail.com>
2025-02-27 19:50:25 -0800

    Increase iterations and randomize order of tests

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 class TestClass {
-    constructor(value) {
-        this._value = value;
-    }
+    _value = 'test';
 
-    get value() {
+    get value () {
         return this._value;
     }
 
@@ -12,30 +10,65 @@ class TestClass {
     }
 }
 
-const iterations = 1000000;
-const testInstance = new TestClass(42);
+const iterations = 100000000;
+const testInstance = new TestClass();
 
-// Measure property access time
-let start = performance.now();
-for (let i = 0; i < iterations; i++) {
-    let temp = testInstance._value;
-}
-let propertyAccessTime = performance.now() - start;
+let getterAccessTime = 0, getMethodAccessTime = 0, propertyAccessTime = 0;
+let propertyAccessTimeIterations = 0, getterAccessTimeIterations = 0, getMethodAccessTimeIterations = 0;
+let start;
 
 // Measure getter access time
-start = performance.now();
-for (let i = 0; i < iterations; i++) {
-    let temp = testInstance.value;
-}
-let getterAccessTime = performance.now() - start;
+const measureGetterAccessTime = () => {
+    start = performance.now();
+    for (let i = 0; i < iterations; i++) {
+        const temp = testInstance.value;
+    }
+    getterAccessTime += performance.now() - start;
+    getterAccessTimeIterations += iterations;
+};
 
-// Measure getter access time
-start = performance.now();
-for (let i = 0; i < iterations; i++) {
-    let temp = testInstance.getValue();
-}
-let getMethodAccessTime = performance.now() - start;
+const measureGetMethodAccessTime = () => {
+    start = performance.now();
+    for (let i = 0; i < iterations; i++) {
+        const temp = testInstance.getValue();
+    }
+    getMethodAccessTime += performance.now() - start;
+    getMethodAccessTimeIterations += iterations;
+};
 
-console.log(`Property access time: ${propertyAccessTime} ms`);
-console.log(`Getter access time: ${getterAccessTime} ms`);
-console.log(`Get method access time: ${getMethodAccessTime} ms`);
+const measurePropertyAccessTime = () => {
+    start = performance.now();
+    for (let i = 0; i < iterations; i++) {
+        const temp = testInstance._value;
+    }
+    propertyAccessTime += performance.now() - start;
+    propertyAccessTimeIterations += iterations;
+};
+
+const measures = [measureGetterAccessTime, measureGetMethodAccessTime, measurePropertyAccessTime];
+
+function shuffle (array) {
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+}
+
+for (let i = 0; i < 100; i++) {
+    shuffle(measures);
+
+    measures.forEach(measure => measure());
+}
+
+console.log(`Property access time: ${propertyAccessTime / propertyAccessTimeIterations} ms`);
+console.log(`Getter access time: ${getterAccessTime / getterAccessTimeIterations} ms`);
+console.log(`Get method access time: ${getMethodAccessTime / getMethodAccessTimeIterations} ms`);
